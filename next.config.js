b47bc2bdb93d635649979c59ee6075f5f3ce16d8
JTG947 <johnnythegreat947@gmail.com>
2024-06-22 23:43:10 -0400

    .

--- next.config.js
+++ next.config.js
@@ -59,6 +59,7 @@ module.exports = () => {
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
       dirs: ['app', 'components', 'config', 'hooks', 'lib', 'scripts'],
+      ignoreDuringBuilds: true,
     },
     images: {
       remotePatterns: [
